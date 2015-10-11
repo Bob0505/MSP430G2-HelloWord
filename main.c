5f64a250f4648330d7335d4f632471329c5c074b
bob <bob-0505@hotmail.com>
2015-10-12 00:02:14 +0800

    MSP430 Launchpad Tutorial - Part 2 - Interrupts and timers.

--- main.c
+++ main.c
@@ -2,70 +2,160 @@
 #include <msp430g2553.h>
 #include "main.h"
 
-int GPIORW(
-	uint8_t selectedPort,
-	uint8_t selectedPins,
-	uint8_t DIR_InOut,
-	uint8_t HL
+void FunSelGPIO(
+	uint8_t		selectedPort,
+	uint8_t		selectedPins
 ){
-	uint8_t status = Status_Success;
-
 	//8.2.5 Function Select Registers PxSEL and PxSEL2
 	//PxSEL2  PxSEL
 	//	0 		0	I/O function is selected
-	P1SEL	&= ~selectedPins;
-	P1SEL2	&= ~selectedPins;
+	switch (selectedPort)
+	{
+		case GPIO_Port1:
+			P1SEL	&= ~selectedPins;
+			P1SEL2	&= ~selectedPins;
+			break;
+
+		case GPIO_Port2:
+			break;
+
+		default:
+			break;
+	}
+	return;
+}
+
+void GPIO_SetOutput(
+	uint8_t			selectedPort,
+	uint8_t			selectedPins,
+	GPIO_SETTING	*gpiosettings
+){
+	FunSelGPIO(selectedPort, selectedPins);
 
 	switch (selectedPort)
 	{
 		case GPIO_Port1:
-			//8.2.3 Direction Registers PxDIR
-			//	Bit = 0: The port pin is switched to input direction
-			//	Bit = 1: The port pin is switched to output direction
-			if(GPIO_Input == DIR_InOut)
-			{
-				P1DIR &= ~selectedPins;
-				//8.2.4 Pullup/Pulldown Resistor Enable Registers PxREN
-				//	Bit = 0: Pullup/pulldown resistor disabled
-				//	Bit = 1: Pullup/pulldown resistor enabled
-				P1REN |=  selectedPins;
-
-			}
-			else if(GPIO_Output == DIR_InOut)
-			{
-				P1DIR |=  selectedPins;
-				//8.2.2 Output Registers PxOUT
-				//	Bit = 0: The output is low
-				//	Bit = 1: The output is high
-				if(GPO_Low == HL)
-					P1OUT &= ~selectedPins;
-				else if(GPO_High == HL)
-					P1OUT |=  selectedPins;
-			}
+			P1DIR |=  selectedPins;
+			if(GPIO_Low == gpiosettings->r.output)
+				P1OUT &= ~selectedPins;
+			else // GPIO_High
+				P1OUT |=  selectedPins;
 			break;
+
 		case GPIO_Port2:
-
 			break;
-		default:
 
+		default:
 			break;
 	}
-
-	return status;
+	return;
 }
 
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+void GPIO_SetInput(
+	uint8_t			selectedPort,
+	uint8_t			selectedPins,
+	GPIO_SETTING	*gpiosettings
+){
+	FunSelGPIO(selectedPort, selectedPins);
 
-    GPIORW( GPIO_Port1, BIT0, GPIO_Output, GPO_Low);	// initialize P1.0 output Low
-    GPIORW( GPIO_Port1, BIT6, GPIO_Output, GPO_High);	// initialize P1.6 output High
+	P1DIR &= ~selectedPins;
+
+	//8.2.4 Pullup/Pulldown Resistor Enable Registers PxREN
+	if(Disable == gpiosettings->r.pullren)
+	{
+		P1REN &= ~selectedPins;
+	} else	{	// Enable
+		P1REN |=  selectedPins;
+
+		if(GPI_PullDown == gpiosettings->r.output)
+			P1OUT &= ~selectedPins;
+		else	//GPI_PullUp
+			P1OUT |= selectedPins;
+	}
+
+	if(Disable == gpiosettings->r.intr_en)
+	{
+		P1IE &= ~selectedPins;
+	} else {	// Enable
+		P1IE |= selectedPins;
+
+		if(GPI_intr_Rising == gpiosettings->r.intr_es)
+			P1IES &= ~selectedPins;
+		else	// GPI_intr_Falling
+			P1IES |= selectedPins;
+
+		P1IFG &= ~selectedPins;			// P1.3 IFG cleared
+	}
+	return;
+}
+
+
+//P1.0	Red LED
+//P1.3	Button
+//P1.6	Green LED
+
+int main(void) {
+
+	GPIO_SETTING	gpiosettings;
+
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+#if	0
+	// initialize P1.0 output Low and P1.6 output High
+	gpiosettings.r.Direction= GPIO_Output;
+	gpiosettings.r.output	= GPIO_Low;
+    GPIO_SetOutput( GPIO_Port1, BIT0, &gpiosettings);
+	gpiosettings.r.output	= GPIO_High;
+    GPIO_SetOutput( GPIO_Port1, BIT6, &gpiosettings);
 
 	while (1) {
 		P1OUT ^= BIT0;				// Toggle LED on P1.0
 		P1OUT ^= BIT6;				// Toggle LED on P1.6
 		__delay_cycles(100000);		// Wait ~100ms at default DCO of ~1MHz
 	}
+}
+#else
+	CCTL0 = CCIE;					// CCR0 interrupt enabled
+	//12.3.1 TACTL, Timer_A Control Register
+	TACTL = TASSEL_2 | ID_3 | MC_1;	// SMCLK, /8, Up mode
+	CCR0 =  10000;					// 12.5 Hz
 
+	// Shut down everything
+	P1OUT &= 0x00;
+	P1DIR &= 0x00;
 
+	// initialize P1.0 and P1.6 pins output Low the rest are input
+	gpiosettings.r.Direction= GPIO_Output;
+	gpiosettings.r.output	= GPIO_Low;
+    GPIO_SetOutput( GPIO_Port1, BIT0, &gpiosettings);
+    GPIO_SetOutput( GPIO_Port1, BIT6, &gpiosettings);
+
+	// initialize P1.3 pin input PullUp Falling trigger
+	gpiosettings.r.Direction= GPIO_Input;
+    gpiosettings.r.pullren	= Enable;
+    gpiosettings.r.output	= GPI_PullUp;
+    gpiosettings.r.intr_en	= Enable;
+    gpiosettings.r.intr_es	= GPI_intr_Falling;
+    GPIO_SetInput( GPIO_Port1, BIT3, &gpiosettings);
+
+	// Enter LPM0 w/ interrupt
+	_BIS_SR(CPUOFF + GIE);
+
+//	while(1){}	//Loop forever, we work with interrupts!
 	return 0;
 }
+
+// Timer A0 interrupt service routine
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+{
+	P1OUT ^= BIT0;	// Toggle P1.0
+}
+
+// Port 1 interrupt service routine
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+	P1OUT ^= BIT6;	// Toggle P1.6
+	P1IFG &= ~BIT3;	// P1.3 IFG cleared
+}
+#endif
--- main.h
+++ main.h
@@ -8,13 +8,34 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-typedef unsigned char uint8_t ;
+typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
+
+//Table 8-2. Digital I/O Registers
+typedef union {
+	uint8_t	ul;
+	struct {
+		uint8_t	input	: 1;	// 0	input value
+		uint8_t	output	: 1;	// 1	input pulldir/output value
+		uint8_t	Direction:1;	// 2	input/output direction
+		uint8_t	intr_ifg: 1;	// 3	interrupt flag
+		uint8_t	intr_es	: 1;	// 4	interrupt edge select registers
+		uint8_t	intr_en	: 1;	// 5	interrupt enable
+		uint8_t	pullren	: 1;	// 6	input Pullup/pulldown resistor enable
+		uint8_t	reserve : 1;	// 7
+  } r;
+} GPIO_SETTING;
 
 enum MSP430_STATUS
 {
 	Status_Success = 0x00
+};
+
+enum MSP430_EnDis
+{
+	Disable = 0x00,
+	Enable	= 0x01
 };
 
 enum MSP430_GPIO_PORT
@@ -23,16 +44,34 @@ enum MSP430_GPIO_PORT
 	GPIO_Port2 = 0x02
 };
 
+//8.2.2 Output Registers PxOUT
+//	Bit = 0: The output is low
+//	Bit = 1: The output is high
+enum MSP430_GPIO_VALUE
+{
+	GPIO_Low	= 0x00,
+	GPIO_High	= 0x01
+};
+
+enum MSP430_GPI_PULLDIR
+{
+	GPI_PullDown= 0x00,
+	GPI_PullUp	= 0x01
+};
+
+//8.2.3 Direction Registers PxDIR
+//	Bit = 0: The port pin is switched to input direction
+//	Bit = 1: The port pin is switched to output direction
 enum MSP430_GPIO_DIR
 {
 	GPIO_Input	= 0x00,
 	GPIO_Output	= 0x01
 };
 
-enum MSP430_GPO_HL
+enum MSP430_GPI_INTR_ES
 {
-	GPO_Low	 = 0x00,
-	GPO_High = 0x01
+	GPI_intr_Rising	= 0x00,
+	GPI_intr_Falling= 0x01
 };
 
 #endif /* MAIN_H_ */
